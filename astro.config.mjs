9c0b9061ab20ee0ac27f5c63a7a93ad2333d06bf
夏叶 <xiaye@msn.com>
2025-10-11 21:18:52 +0800

    fix: demo url

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://demo-firefly.cuteleaf.com/",
+	site: "https://demo-firefly.netlify.app/",
 
 	base: "/",
 	trailingSlash: "always",
