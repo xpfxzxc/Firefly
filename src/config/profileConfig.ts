97092d957e138fa9b7b67be9bcf027d73df9d75d
xpfxzxc <xpfxzxc@gmail.com>
2025-11-18 17:15:38 +0800

    feat: 完善个人基本资料（头像、名称、介绍、社交媒体链接）
    - 更新个人基本信息（名称、介绍）
    - 替换个人头像图片
    - 调整现有社交媒体链接
    - 新增两个社交媒体平台链接

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,18 +2,28 @@ import type { ProfileConfig } from "../types/config";
 
 export const profileConfig: ProfileConfig = {
   avatar: "/assets/images/avatar.webp",
-  name: "Firefly",
-  bio: "Hello, I'm Firefly.",
+  name: "xpfxzxc",
+  bio: "不愿沉溺于无力感，正试着将碎片化的思考转化成可见的成长足迹。",
   links: [
     {
-      name: "Bilibli",
-      icon: "fa6-brands:bilibili",
-      url: "https://space.bilibili.com/38932988",
+      name: "Email",
+      icon: "material-symbols:mail",
+      url: "mailto:xpfxzxc@gmail.com",
     },
     {
       name: "GitHub",
       icon: "fa6-brands:github",
-      url: "https://github.com/CuteLeaf",
+      url: "https://github.com/xpfxzxc",
+    },
+    {
+      name: "Bilibli",
+      icon: "fa6-brands:bilibili",
+      url: "https://space.bilibili.com/12533717",
+    },
+    {
+      name: "Steam",
+      icon: "fa6-brands:steam",
+      url: "https://steamcommunity.com/profiles/76561198327614842/",
     },
   ],
 };
